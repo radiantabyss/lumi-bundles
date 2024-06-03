3f58cb45cc6db4c6819772ec4f2a8bc71a2f17b6
Robert B <robert@planetweb.ro>
2024-06-04 00:25:56 +0300

    'v1.0.1'

--- vue-ssr/ssr/resources/views/components/post.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="post">
-    <div class="post__gallery">
-        @foreach ( $item->media as $media_item )
-            @include('/components/media', ['item' => $media_item])
-        @endforeach
-    </div>
-
-    @include('/components/post-details')
-</div>
